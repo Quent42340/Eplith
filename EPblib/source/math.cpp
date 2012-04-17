e0e4a440f3e8ed0b30b824c7483848befdf2ce5e
Quentin BAZIN <quent42340@gmail.com>
2012-04-17 20:03:05 +0200

    Working on Eplith base library (EPblib): Math.

--- EPblib/source/math.cpp
+++ EPblib/source/math.cpp
@@ -24,16 +24,91 @@
 
 using namespace std;
 
-class CosExpression : public Expression {
+typedef enum {
+	mCOS,
+	mSIN,
+	mTAN,
+	mACOS,
+	mASIN,
+	mATAN,
+	mATAN2,
+	mCOSH,
+	mSINH,
+	mTANH,
+	mSQRT,
+	mEXP,
+	mFREXP,
+	mLDEXP,
+	mLOG,
+	mLOG10,
+	mCEIL,
+	mFLOOR,
+	mFABS,
+	mFMOD
+} MathFunc;
+
+class MathExpression : public Expression {
 	public:
-		CosExpression(Expression *exp) { m_exp = exp; }
-		~CosExpression() { delete m_exp; }
+		MathExpression(Expression *exp, MathFunc func) { m_exp = exp; m_exp2 = 0; m_func = func; }
+		MathExpression(Expression *exp, Expression *exp2, MathFunc func) { m_exp = exp; m_exp2 = exp2 m_func = func; }
+		~MathExpression() { delete m_exp; if(m_exp2) delete m_exp2; }
 		
-		Value *evaluate() {	return new Value(cos(getNumVal(m_exp->evaluate()))); }
+		Value *evaluate() {
+			switch(m_func) {
+				case mCOS:
+					return new Value(cos(getNumVal(m_exp->evaluate())));
+				case mSIN:
+					return new Value(sin(getNumVal(m_exp->evaluate())));
+				case mTAN:
+					return new Value(tan(getNumVal(m_exp->evaluate())));
+				case mACOS:
+					return new Value(acos(getNumVal(m_exp->evaluate())));
+				case mASIN:
+					return new Value(asin(getNumVal(m_exp->evaluate())));
+				case mATAN:
+					return new Value(atan(getNumVal(m_exp->evaluate())));
+				case mATAN2:
+					if(!m_exp2) yyerror("Unexpected error");
+					return new Value(atan2(getNumVal(m_exp->evaluate()), getNumVal(m_exp2->evaluate())));
+				case mCOSH:
+					return new Value(cosh(getNumVal(m_exp->evaluate())));
+				case mSINH:
+					return new Value(sinh(getNumVal(m_exp->evaluate())));
+				case mTANH:
+					return new Value(tanh(getNumVal(m_exp->evaluate())));
+				case mSQRT:
+					return new Value(sqrt(getNumVal(m_exp->evaluate())));
+				case mEXP:
+					return new Value(exp(getNumVal(m_exp->evaluate())));
+//				case mFREXP:
+//					return new Value(exp(getNumVal(m_exp->evaluate())));
+				case mLDEXP:
+					if(!m_exp2) yyerror("Unexpected error");
+					return new Value(ldexp(getNumVal(m_exp->evaluate()), getNumVal(m_exp2->evaluate())));
+				case mLOG:
+					return new Value(log(getNumVal(m_exp->evaluate())));
+				case mLOG10:
+					return new Value(log10(getNumVal(m_exp->evaluate())));
+//				case mMODF:
+//					return new Value(modf(getNumVal(m_exp->evaluate())));
+				case mCEIL:
+					return new Value(ceil(getNumVal(m_exp->evaluate())));
+				case mFLOOR:
+					return new Value(floor(getNumVal(m_exp->evaluate())));
+				case mFABS:
+					return new Value(fabs(getNumVal(m_exp->evaluate())));
+				case mFMOD:
+					return new Value(fmod(getNumVal(m_exp->evaluate()), getNumVal(m_exp2->evaluate())));
+				default:
+					yyerror("Unexpected error");
+			}
+		}
 		void doExp() {}
 		
 	private:
 		Expression *m_exp;
+		Expression *m_exp2;
+		MathFunc m_func;
 };
 
 void EPblib_initMath() {
@@ -44,11 +119,102 @@ void EPblib_initMath() {
 	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
 	cosArgs->push_back(new VarExpression("x"));
 	vector<Expression*> *cosStmts = new vector<Expression*>;
-	cosStmts->push_back(new ReturnExpression(new CosExpression((*cosArgs)[0])));
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
+	endScope();
+	Value *cos = new Value(new Function(cosArgs, cosStmts));
+	
+	// Math.sin
+	beginScope(stFUNC);
+	vector<VarExpression*> *sinArgs = new vector<VarExpression*>;
+	sinArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *sinStmts = new vector<Expression*>;
+	sinStmts->push_back(new ReturnExpression(new MathExpression((*sinArgs)[0], mSIN)));
+	endScope();
+	Value *sin = new Value(new Function(sinArgs, sinStmts));
+	
+	// Math.tan
+	beginScope(stFUNC);
+	vector<VarExpression*> *tanArgs = new vector<VarExpression*>;
+	tanArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *tanStmts = new vector<Expression*>;
+	tanStmts->push_back(new ReturnExpression(new MathExpression((*tanArgs)[0], mTAN)));
+	endScope();
+	Value *tan = new Value(new Function(tanArgs, tanStmts));
+	
+	// Math.acos
+	beginScope(stFUNC);
+	vector<VarExpression*> *acosArgs = new vector<VarExpression*>;
+	acosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *acosStmts = new vector<Expression*>;
+	acosStmts->push_back(new ReturnExpression(new MathExpression((*acosArgs)[0], mACOS)));
+	endScope();
+	Value *acos = new Value(new Function(acosArgs, acosStmts));
+	
+	// Math.cos
+	beginScope(stFUNC);
+	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
+	cosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *cosStmts = new vector<Expression*>;
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
+	endScope();
+	Value *cos = new Value(new Function(cosArgs, cosStmts));
+	
+	// Math.cos
+	beginScope(stFUNC);
+	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
+	cosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *cosStmts = new vector<Expression*>;
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
+	endScope();
+	Value *cos = new Value(new Function(cosArgs, cosStmts));
+	
+	// Math.cos
+	beginScope(stFUNC);
+	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
+	cosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *cosStmts = new vector<Expression*>;
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
+	endScope();
+	Value *cos = new Value(new Function(cosArgs, cosStmts));
+	
+	// Math.cos
+	beginScope(stFUNC);
+	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
+	cosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *cosStmts = new vector<Expression*>;
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
+	endScope();
+	Value *cos = new Value(new Function(cosArgs, cosStmts));
+	
+	// Math.cos
+	beginScope(stFUNC);
+	vector<VarExpression*> *cosArgs = new vector<VarExpression*>;
+	cosArgs->push_back(new VarExpression("x"));
+	vector<Expression*> *cosStmts = new vector<Expression*>;
+	cosStmts->push_back(new ReturnExpression(new MathExpression((*cosArgs)[0], mCOS)));
 	endScope();
 	Value *cos = new Value(new Function(cosArgs, cosStmts));
 	
 	Math_elements.insert(Math_elements.end(), pair<string, Value*>("cos", cos));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("sin", sin));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("tan", tan));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("acos", acos));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("asin", asin));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("atan", atan));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("atan2", atan2));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("cosh", cosh));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("sinh", sinh));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("tanh", tanh));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("sqrt", sqrt));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("exp", exp));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("frexp", frexp));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("ldexp", ldexp));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("log", log));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("log10", log10));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("ceil", ceil));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("floor", floor));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("fabs", fabs));
+	Math_elements.insert(Math_elements.end(), pair<string, Value*>("fmod", fmod));
 	Math_elements.insert(Math_elements.end(), pair<string, Value*>("pi", new Value(PI)));
 	
 	Variable *Math = new Variable("Math", new Value(Math_elements));
